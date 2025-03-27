fcaffe322fe7e2762ef6c8672f14c1bd902ca793
Anibal Copitan <github@pprios.com>
2025-03-27 17:21:43 -0500

    update: adding logs

--- script.sh
+++ script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo "=============================="
-echo "Executing each 1 hour The beep"
+echo "Executing The beep"
+echo "File: $0"
 echo "=============================="
+echo "Script begin: $(date '+%H:%M:%S')"
 
 # Note: It's required put the absolute path for this clock work!
+/usr/bin/play /home/anb/sites/anibal/local.anibalcopitan.com/projects/clock-linux-pc/beep.mp3
 
-play /home/anb/sites/anibal/local.anibalcopitan.com/projects/clock-linux-pc/beep.mp3
-
+echo "Script end: $(date '+%H:%M:%S')"
